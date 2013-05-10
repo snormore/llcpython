330baf49da37a49b1c148e30a12df1b3521fce19
Tavis Rudd <tavis@damnsimple.com>
2013-05-10 13:53:45 -0700

    tweaking exercises. almost done with the final exercises

--- llc_python.py
+++ llc_python.py
@@ -15,13 +15,20 @@ client = pytumblr.TumblrRestClient(
     access_token_key, access_token_secret)
 
 def hello_llc(blog_text):
-    """hello_llc(blog_text) :: string -> None. Side-effect of creating a tumblr post.
+    """Uploads `blog_text` to our shared class tumblr blog http://llcpythonyvr.tumblr.com/
 
-    Upload `blog_text` to our shared class tumblr blog http://llcpythonyvr.tumblr.com/
+    hello_llc(blog_text) :: string -> None. Side-effect of creating a tumblr post.
     """
     result = client.create_text(hostname, body=blog_text)
     return HTML(
-       '''Uploading now.
-       Click <a href="http://llcpythonyvr.tumblr.com/" target="new">here</a> for the post.
-       Tubmlr replied "%s".'''
+       '''<div style="padding: 15px">Uploading now.
+       Click <a href="http://llcpythonyvr.tumblr.com/"
+       target="new">here</a> to see your post!
+       Tubmlr replied with some gobbledygook "%s" and an astro cat.</div>
+       <img
+       src="http://24.media.tumblr.com/b350f252a9460c6f9f3bb4af7702a566/tumblr_mllsjv1Qo01rmn2exo1_500.gif"
+       />'''
        % result)
+
+def mind_blown():
+    return HTML('<img src="http://i.imgur.com/UmpOi.gif" />')
